00e95151a7b64d9f6462f6ea52f056496bb658d3
rikkelose <rikkelose@gmail.com>
2015-11-19 14:16:04 +0100

    adding missing index.js

--- src/__tests__/test.js
+++ src/__tests__/test.js
@@ -9,9 +9,8 @@ describe('Test OpenHoldingStatus getHolding', () => {
   it('Dummy', function(done) {
 
     done();
-
-    /*
-    this.timeout(5000);
+    
+    /*this.timeout(5000);
     setTimeout(done, 5000);
     const config = {
       wsdl: 'http://openholdingstatus.addi.dk/2.2/?wsdl'
@@ -20,14 +19,15 @@ describe('Test OpenHoldingStatus getHolding', () => {
     OpenHoldingStatus.init(config);
     let result = OpenHoldingStatus.getHolding({
       responderId: '710100',
-      pid: '870970-basis:43512447'
+      pid: '870970-basis:43512449'
     });
 
     result.then(function (holdingsResult) {
+      console.log(holdingsResult);
       assert.equal(holdingsResult['responder']['willLend'], 'true');
       done();
-    });
-    */
+    });*/
+    
 
   });
 });
