f9ba9d841fe1e24490085b48d3d985d24f6c828f
Søren Vibjerg <vibjerg@gmail.com>
2015-11-30 15:33:55 +0100

    export default factory function to unify dbc service client API

--- src/client.js
+++ src/client.js
@@ -2,44 +2,29 @@
 
 import * as BaseSoapClient from 'dbc-node-basesoap-client';
 
-let wsdl = null;
-let defaults = {};
-
-/**
- * Retrieves data from the webservice based on the parameters given
- *
- * @param {Object} params Parameters for the request
- * @return {Promise}
- */
-
-function sendHoldingsRequest(params) {
-  let holdings = BaseSoapClient.client(wsdl, defaults, '');
-  return holdings.request('holdings', params, null, true);
-}
-
 /**
  * Constructs the object of parameters for OpenHoldingStatus holdings request.
  *
  * @param {Object} values Object with the necessary parameters
  * @return {Promise}
  */
-export function getHolding(values) {
+function getHolding(client, values) {
   const params = {
     lookupRecord: {
       responderId: values.responderId,
       pid: values.pid
     }
   };
-  return sendHoldingsRequest(params);
+  return client.request('holdings', params, null, true);
 }
 
-export function init(config = null) {
+export default function OpenHoldingstatus(config = null) {
   if (!config || !config.wsdl) {
     throw new Error('Expected config object but got null or no wsdl provided');
   }
-  wsdl = config.wsdl;
-}
+  const holdingsClient = BaseSoapClient.client(config.wsdl, {}, '');
 
-export const METHODS = {
-  getHolding: getHolding
-};
+  return {
+    getHolding: getHolding.bind(null, holdingsClient)
+  }
+}
